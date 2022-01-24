2484d8ce94e87093cd672d74c74d6c2742890271
terrotar <pedro_guedes93@hotmail.com>
2022-01-24 15:56:14 -0300

    Set reload=True to helps with development

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 
 import uvicorn
-from api.main import api
 
 
 if __name__ == "__main__":
-    uvicorn.run(api, host="127.0.0.1", port=8000)
+    uvicorn.run('app.main:api',
+                host="127.0.0.1",
+                port=8000,
+                reload=True)
