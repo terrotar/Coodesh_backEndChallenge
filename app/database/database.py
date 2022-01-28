37bec07fa59e310c887860436a61f0cd971dc7e1
terrotar <pedro_guedes93@hotmail.com>
2022-01-28 00:53:39 -0300

    Added Heroku PostgreSQL

--- app/database/database.py
+++ app/database/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/Space_Flight_News_Challenge"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/Space_Flight_News_Challenge"
+SQLALCHEMY_DATABASE_URL = 'postgres://oelucwrdopakiw:22cb024f07283c0d0982164786374b40bd4f0b7107d4ba3e4ff19fdb6203a0ff@ec2-54-224-64-114.compute-1.amazonaws.com:5432/d8r0usr9c6l03k'
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
